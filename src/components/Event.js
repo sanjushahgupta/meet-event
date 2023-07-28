59d5625a964571f71b6ea050bea18d1c23fb6b87
Sanju Shah <sanjushahgupta@gmail.com>
2023-07-28 12:23:05 +0200

    test:  event information displays correctly

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,11 @@
-const Event = () => {
+const Event = ({event}) => {
     return (
-      <li></li>
-    );
+     <div>
+      <h5>{event.summary}</h5>
+      <h5>{event.location}</h5>
+      <h5>{event.created}</h5>
+</div>    
+);
   }
   
   export default Event;
