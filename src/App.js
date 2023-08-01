d1669cf411f0726b4367afd677b35a7ea7d6ec16
Sanju Shah <sanjushahgupta@gmail.com>
2023-08-01 19:20:44 +0200

    test: select a city from the suggested list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch'
 import NumberOfEvents from './components/NumberOfEvents'
+import { useState, useEffect} from 'react';
+import { getEvents, extractLocations} from './api';
 
 import './App.css';
 
 function App() {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+   const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  }
+  
   return (
     <div className="App">
-    <EventList />
-    <CitySearch/> 
-    <NumberOfEvents/>
+    <NumberOfEvents />
+    <CitySearch allLocations={allLocations} />
+    <EventList events={events}/>
   </div>
   );    
 }
+
+
 
 export default App;
--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,10 +1,15 @@
 
-  import { useState } from "react";
+  import { useState, useEffect } from "react";
 
   const CitySearch=({ allLocations })=>{
       const [showSuggestions, setShowSuggestions] = useState(false);
       const [query, setQuery] = useState("");
       const [suggestions, setSuggestions] = useState([]);
+      
+      useEffect(() => {
+        setSuggestions(allLocations);
+      }, [`${allLocations}`]);
+
       const handleInputChanged = (event) => {
         const value = event.target.value;
         const filteredLocations = allLocations ? allLocations.filter((location) => {
@@ -19,7 +24,7 @@
         setQuery(value);
         setShowSuggestions(false); 
       };
-      
+  
       return(
         <div id="city-search">
         <input
