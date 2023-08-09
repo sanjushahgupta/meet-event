608e161b911542f60032e1f24f5f78e153e9c99e
Sanju Shah <sanjushahgupta@gmail.com>
2023-08-09 15:04:13 +0200

    feat: add alert message when there is no internet

--- src/components/alert.js
+++ src/components/alert.js
@@ -46,4 +46,11 @@ class ErrorAlert extends Alert{
     
 }
 
-export { InfoAlert, ErrorAlert}
+class WarningAlert extends Alert{
+    constructor(props) {
+        super(props)
+        this.color = 'rgb(255,255,255)'
+        this.bgColor ='rgb(230, 0, 0)'
+    }
+}
+export { InfoAlert, ErrorAlert, WarningAlert}
